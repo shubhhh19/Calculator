bc36fa35084ec26737a505295f2235d9b4b37faf
shubhhh19 <ssoni7735@conestogac.on.ca>
2023-12-26 15:12:35 -0500

    Added choices prompt for user

--- Calculator.py
+++ Calculator.py
@@ -14,3 +14,23 @@ def div(a, b):
     result = a / b
     print(str(a) + " / " + str(b) + " = " + str(result))
 
+print("Welcome to Shubh's Companion!")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+print("5. Exit")
+
+choice = input("Enter your choice: ")
+
+if choice == "1":
+    print("Addition")
+    a = int(input("Enter first operand: "))
+    b = int(input("Enter second operand: "))
+    add(a, b)
+elif choice == "2":
+    print("Subtraction")
+    a = int(input("Enter first operand: "))
+    b = int(input("Enter second operand: "))
+    sub(a, b)
+    
