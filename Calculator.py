99b68f6339d91151dcc65aead1f53d8cc0f2b732
shubhhh19 <ssoni7735@conestogac.on.ca>
2023-12-26 15:22:21 -0500

    Added loop to call it everytime until user exits

--- Calculator.py
+++ Calculator.py
@@ -1,36 +1,49 @@
 def add(a, b):
     result = a + b
-    print(str(a) + " + " + str(b) + " = " + str(result))
+    print("Answer: " + str(a) + " + " + str(b) + " = " + str(result) + "\n")
     
 def sub(a, b):
     result = a - b
-    print(str(a) + " - " + str(b) + " = " + str(result))
+    print("Answer: " + str(a) + " - " + str(b) + " = " + str(result) + "\n")
 
 def mul(a, b):
     result = a * b
-    print(str(a) + " * " + str(b) + " = " + str(result))
+    print("Answer: " + str(a) + " * " + str(b) + " = " + str(result) + "\n")
 
 def div(a, b):
     result = a / b
-    print(str(a) + " / " + str(b) + " = " + str(result))
+    print("Answer: " + str(a) + " / " + str(b) + " = " + str(result) + "\n")
 
 print("Welcome to Shubh's Companion!")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
-print("5. Exit")
+while True:
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
 
-choice = input("Enter your choice: ")
+    choice = int(input("Enter your choice: "))
 
-if choice == "1":
-    print("Addition")
-    a = int(input("Enter first operand: "))
-    b = int(input("Enter second operand: "))
-    add(a, b)
-elif choice == "2":
-    print("Subtraction")
-    a = int(input("Enter first operand: "))
-    b = int(input("Enter second operand: "))
-    sub(a, b)
-    
+    if choice == 1:
+        print("Addition")
+        a = int(input("Enter first operand: "))
+        b = int(input("Enter second operand: "))
+        add(a, b)
+    elif choice == 2:
+        print("Subtraction")
+        a = int(input("Enter first operand: "))
+        b = int(input("Enter second operand: "))
+        sub(a, b)
+    elif choice == 3:
+        print("Multiplication")
+        a = int(input("Enter first operand: "))
+        b = int(input("Enter second operand: "))
+        mul(a, b)
+    elif choice == 4:
+        print("Division")
+        a = int(input("Enter first operand: "))
+        b = int(input("Enter second operand: "))
+        div(a, b)
+    elif choice == 5:
+        print("Exited.")
+        quit()
