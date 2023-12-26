925d38514b40bec6950449c0cf7db9a5fc9da44f
shubhhh19 <ssoni7735@conestogac.on.ca>
2023-12-26 15:04:58 -0500

    Added basic functions for calculation

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    result = a + b
+    print(str(a) + " + " + str(b) + " = " + str(result))
+    
+def sub(a, b):
+    result = a - b
+    print(str(a) + " - " + str(b) + " = " + str(result))
+
+def mul(a, b):
+    result = a * b
+    print(str(a) + " * " + str(b) + " = " + str(result))
+
+def div(a, b):
+    result = a / b
+    print(str(a) + " / " + str(b) + " = " + str(result))
+
